67a6ada533de93516c80be180e5582f35e0272fa
Rahul More <more03625@gmail.com>
2023-09-17 12:55:22 +0530

    ADD: DSA - LinkedList - create and display linked list

---
+++ dsa/js/linkedList/createLinkedList.js
@@ -0,0 +1,49 @@
+const createNode = (data) => {
+    return {
+        data: data,
+        next: null
+    };
+}
+
+const append = (head, data) => {
+    const newNode = createNode(data);
+
+    // It first checks if the head of the list is null. If it is, then the list is empty and the new node becomes the head of the list.
+    if (!head) {
+        return newNode;
+    }
+
+    /*
+        * The function iterates over the list until it finds the last node (which is the node whose next property is null).
+        * Then, the function sets the next property of the last node to point to the new node. 
+    */
+
+    let current = head; // Head is Object so it will be copied by reference.
+
+    while (current.next) {
+        current = current.next;
+    }
+
+    /*
+        * NOTE: when you are setting `current.next` the `head` will be updated because `objects are passed by reference`
+    */
+    current.next = newNode; // Set the 'next' property of the last node to the new node.
+    return head; // Return the updated head of the list.
+
+}
+
+const display = (head) => {
+    let current = head;
+    while (current) {
+        current = current.next;
+    }
+}
+
+// Example usage:
+let head = null;
+head = append(head, 1); // head = {data:1, next: null}
+head = append(head, 2); //
+head = append(head, 3);
+
+console.log("Linked List =====>  ", head);
+// display(head);
