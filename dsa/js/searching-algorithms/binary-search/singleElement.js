7172763cad9954fefef23d2687434d42df6d4130
Rahul More <more03625@gmail.com>
2025-02-24 10:55:04 +0530

    ADD: single element

--- dsa/js/searching-algorithms/binary-search/singleElement.js
+++ dsa/js/searching-algorithms/binary-search/singleElement.js
@@ -1,7 +1,25 @@
 const singleElement = (arr) => {
+    let left = 0;
+    let right = arr.length - 1;
 
+    while (left <= right) {
+        const mid = Math.floor(left + ((right - left) / 2))
 
+        if (arr[mid] !== arr[mid - 1] && arr[mid] !== arr[mid + 1]) {
+            return arr[mid]
+        }
+
+        if ((mid % 2 === 1 && arr[mid] === arr[mid - 1]) || (mid % 2 === 0 && arr[mid] === arr[mid + 1])) {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+
+    return -1
 }
 
-const arr = [1, 1, 2, 3, 3, 4, 4, 8, 8];
+const arr = [1, 1, 2, 3, 3, 4, 4, 8, 8]; // ODD
+
+// const arr = [1, 1, 2, 2, 3, 4]; EVEN
 console.log('singleElement ===>', singleElement(arr));
